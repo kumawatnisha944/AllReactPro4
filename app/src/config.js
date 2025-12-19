9f40c066cb435b8573a56f7526bbdb133f7ee1ea
Nisha Kumawat <kumawatnisha9091@gmail.com>
2025-12-19 16:03:09 +0530

    Update frontend to use live backend URL

--- app/src/config.js
+++ app/src/config.js
@@ -1,5 +1,5 @@
-
 export const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:9000"
-    : "https://food-backend.onrender.com";
+    : "https://food-backend-4k02.onrender.com";
+
